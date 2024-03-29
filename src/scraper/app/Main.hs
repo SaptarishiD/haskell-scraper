9131c2ec061a6b962a4cd04d936afcc1a94f7c8b
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2024-03-29 16:06:04 +0530

    made stack project

---
+++ src/scraper/app/Main.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Data.Text
+import Data.Char (isSpace)         
+import Text.HTML.Scalpel
+
+
+trim :: String -> String
+trim = f . f
+  where f = Prelude.reverse . Prelude.dropWhile isSpace
+
+scraper :: IO (Maybe Text)
+scraper = scrapeURL "https://eli.thegreenplace.net/2018/type-erasure-and-reification/" heading
+
+  where
+    heading :: Scraper Text Text
+    heading = text "h1"
+
+main :: IO ()
+main = do
+  result <- scraper
+  case result of
+    Just x  -> print (trim (unpack x))
+    Nothing -> print "Didn't find the necessary items."
