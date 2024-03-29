f500ad15432791eba56566f2d0cf354a20a7d4e7
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2024-03-29 17:10:47 +0530

    h1, h2, p and pre tags separately

--- src/scraper/app/Main.hs
+++ src/scraper/app/Main.hs
@@ -5,25 +5,59 @@ module Main (main) where
 import Data.Text
 import Data.Char (isSpace)         
 import Text.HTML.Scalpel
-import Text.Read (Lexeme(String))
+
 
 -- trims the whitespace and newlines from a string. Possibly won't use this so that the formatting in the output is nice cause of the spaces
 trim :: String -> String
 trim = f . f
   where f = Prelude.reverse . Prelude.dropWhile isSpace
 
-myScraper :: IO (Maybe [String])
-myScraper = scrapeURL "https://eli.thegreenplace.net/2018/type-erasure-and-reification/" headings
-  where
+headingScraper :: IO (Maybe [String])
+headingScraper = scrapeURL "https://eli.thegreenplace.net/2018/type-erasure-and-reification/" headings
+  where -- need to be careful about the first header also cause that's h1
     headings :: Scraper String [String] 
-    headings = chroots "h2" heading -- need to be careful about the first header also cause that's h1
+    headings = (:) <$> heading1 <*> chroots "h2" heading2  -- easier way to do this?
 
-    heading :: Scraper String String
-    heading = text "h2"
+    heading1 :: Scraper String String
+    heading1 = text "h1"
+    
+    heading2 :: Scraper String String
+    heading2 = text "h2"
+
+
+paraScraper :: IO (Maybe [String])
+paraScraper = scrapeURL "https://eli.thegreenplace.net/2018/type-erasure-and-reification/" paragraphs
+  where 
+    paragraphs :: Scraper String [String] 
+    paragraphs = chroots "p" paragraph
+    
+    paragraph :: Scraper String String
+    paragraph = text "p"
+
+
+codeScraper :: IO (Maybe [String])
+codeScraper = scrapeURL "https://eli.thegreenplace.net/2018/type-erasure-and-reification/" code_snippets
+  where 
+    code_snippets :: Scraper String [String] 
+    code_snippets = chroots "pre" snippet
+    
+    snippet :: Scraper String String
+    snippet = text "pre"
+
 
 main :: IO ()
 main = do
-  myResult <- myScraper
-  case myResult of
+  headingResult <- headingScraper
+  case headingResult of
     Just x  -> print (Prelude.map trim x)
+    Nothing -> print "Could not find the required elements"
+
+  paraResult <- paraScraper
+  case paraResult of
+    Just x -> print ( Prelude.map (\a -> " \n " ++ a ++ " \n ") x)
+    Nothing -> print "Could not find the required elements"
+
+  codeResult <- codeScraper
+  case codeResult of
+    Just x -> print ( Prelude.map (\a -> " \n " ++ a ++ " \n ") x)
     Nothing -> print "Could not find the required elements"
