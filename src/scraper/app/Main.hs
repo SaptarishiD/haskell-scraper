7a122158ad60f2661154cc8599faa2f4a8dabaf5
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2024-03-31 00:29:54 +0530

    boolean mapping tagopen pre

--- src/scraper/app/Main.hs
+++ src/scraper/app/Main.hs
@@ -22,7 +22,9 @@ trim = dropWhileEnd (== ' ') . dropWhile (== ' ')
 
 -- filterTags :: [Tag String] -> [Tag String]
 
-
+boolToString :: Bool -> String
+boolToString True = "TRUE"
+boolToString False = "FALSE"
 
 main :: IO ()
 main = do
@@ -36,7 +38,19 @@ main = do
 
     html_content <- readFile "output_files/target.html"
     -- print html_content
-    writeFile "output_files/sitetext.txt" (innerText (filter isTagText (parseTags html_content) ) ) 
+    -- let filtered_parsed = filter (\x -> isTagOpen x || isTagClose x || isTagText x) (parseTags html_content)
+    let parsed_tags = parseTags html_content
+    -- print (length parsed_tags)
+    -- get all the elements of the list that are not enclosed between an open pre tag and a close pre tag
+    -- create a list of booleans whose elements are true if for the corresponding element in the above list, we have isTagOpenName = "pre" or isTagCloseName = "pre"
+    -- use the above list to filter the elements of the first list
+    -- let boolean_filter = map (\x -> isTagOpenName "pre" || isTagCloseName "pre") filtered_parsed
+    -- print boolean_filter
+    let bool_mapping = map boolToString ( map (isTagOpenName "pre") parsed_tags ) 
+    print bool_mapping
+    
+
+    -- writeFile "output_files/sitetext.txt" (innerText (filter isTagText (parseTags html_content) ) ) 
     -- print [fromTagText x | x <- (parseTags html_content), isTagText x]
 
 
