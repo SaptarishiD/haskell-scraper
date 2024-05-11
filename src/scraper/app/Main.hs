cff6de8106b43e072ad16363a23b0cfc300f0236
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2024-05-11 17:42:52 +0530

    classified and changed training

--- src/scraper/app/Main.hs
+++ src/scraper/app/Main.hs
@@ -17,34 +17,20 @@ type MyMatrix = [[Int]]
 -- also need to be careful abt hyperparameter tuning and stuff
 -- also can have heuristics in the sense of full stop and colon at end of line meaning natural language and not code
 -- need to compare performance with the test set
-
+-- can also get details about the words and stuff
+-- do cross-validation
 
 main :: IO ()
 main = do
-
-  
-    
-    let code_class = [x | x <- classified, snd x == 0]
-    let lang_class = [x | x <- classified, snd x == 1]
-
-    -- print (map fst code_class)
-    writeFile "output_files/NB_lang_class.txt" (unlines (map fst lang_class))
-    writeFile "output_files/NB_code_class.txt" (unlines (map fst code_class))
-
-
-    -- let classified_lines = read classified :: [(String, String)]
-    -- print classified_lines
-
-    -- let code = [ fst x | x <- fst classified_lines, snd x == 0]
-    -- print code
-
-
-    
 
     let url = "https://eli.thegreenplace.net/2018/type-erasure-and-reification/"
     response_html <- getHTML url
     let parsed_tags = parseTheTags response_html
     let splitted = preProc (splitOnNewline (concat (getWords (getText parsed_tags))))
+
+
+  
+  -- Training ==========================================================================================
 
     natural <- readFile "input/natural_language_text.txt"
     let natural_data = lines natural
@@ -65,32 +51,12 @@ main = do
     let xTrain_natural = myVectorizer vocab (natural_data)
     let yTrain = replicate src_len 0 ++ replicate natural_len 1
 
---     "Unique source code words: "
-
-  -- 1195
-  -- "Natural language words: "
-  -- 11223
-  -- "Total length of vocabulary: "
-  -- 12418
-  -- "Number of source code docs: "
-  -- 1189
-  -- "Number of natural language docs: "
-  -- 7440
-  -- "Total number of documents"
-  -- 8629
-  -- "Number of training documennts: "
-  -- 8629
-  -- "Number of words in vocab: "
-  -- 12418
-  -- let data = [1195,11223,12418,1189,7440,8629]
-
     let sourceCodeMatrix = xTrain_src
     let naturalLanguageMatrix = xTrain_natural
 
     -- DM.Matrix Int -> [Int]
-    let sum_src_cols = (sumCols sourceCodeMatrix)
-    let sum_lang_cols = (sumCols naturalLanguageMatrix)
-
+    let sum_src_cols = sumCols sourceCodeMatrix
+    let sum_lang_cols = sumCols naturalLanguageMatrix
 
     -- maybe change these all to floats or smth explicitly
 
@@ -99,25 +65,49 @@ main = do
 
     let prob_src_prior = (int2Double src_len) / (int2Double src_len + fromIntegral natural_len)
 
-
     -- myVectorizer :: Vocabulary -> [Document] -> Matrix
     -- xTest is of type [[Double]]
-    let xTest = map (map int2Double) (DM.toLists (myVectorizer vocab (splitted)))
 
-    -- print (length (head xTest))
+  -- Training Done ==========================================================================================
 
-    let num_samples = length (splitted)
-    let y = DM.zero 1 num_samples -- here int could be a problem
+
+    lang_test <- readFile "input/lang_test.txt"
+    let lang_test_data = lines lang_test
+    src_test <- readFile "input/code_test.txt"
+    let src_test_data = lines src_test
+
+
+    let xTest_src = map (map int2Double) (DM.toLists (myVectorizer vocab (src_test_data)))
+
+    let xTest_lang = map (map int2Double) (DM.toLists (myVectorizer vocab (lang_test_data)))
+
+
+    let src_test_len = length (xTest_src)
+    let lang_test_len = length (xTest_lang)
+
+    let yTest = replicate src_test_len 0 ++ replicate lang_test_len 1
+
+    print yTest
+    print (sum yTest)
+    print (lang_test_len)
+
+
+
+
+    let xTest = xTest_src ++ xTest_lang
+    -- print xTest
+
+    let y = DM.zero 1 (src_test_len + lang_test_len)
 
     -- these are of type [Double]
     let log_src = map log xgivenY_src
     let log_lang = map log xgivenY_lang
 
-    -- print (map typeOf log_src)
-
-    -- put log_src and long_lang into a new list that has these two lists as it's two elements
     let log_matrix = DM.transpose (DM.fromLists [log_src, log_lang])
+    -- print log_matrix
 
+
+    -- writeFile "output_files/log_matrix.txt" (show log_matrix)
     -- [Double,Double]
     -- print (map typeOf (head (DM.toLists log_matrix)))
 
@@ -128,7 +118,7 @@ main = do
     let logp = log prob_src_prior
     let log_not_p = log (1 - prob_src_prior)
 
-    -- can also just store the matrices and their multiplied results ekbar
+    -- can also just store the matrices and their multiplied results once
     -- presentation mein ofc can't show entire thing so just show results of the trained model on some tests
 
     -- print (DM.nrows prob1) -- 104
@@ -139,23 +129,41 @@ main = do
     let prob2 = map (\x -> x + logp) (head prob1_trans)
     let prob3 = map (\x -> x + log_not_p) (head (tail prob1_trans))
 
-    -- combine the two above lists into one list where the first element is the first list and the second element is the second list
     let combined = [prob2, prob3]
     let combined_mat = DM.transpose (DM.fromLists combined)
 
-    -- print (DM.nrows combined_mat) -- 104
-    -- print (DM.ncols combined_mat) -- 2
+    -- print combined_mat
 
     let final_probs = map (\x -> if (head x) > (head (tail x)) then 0 else 1) (DM.toLists combined_mat)
 
-    let mapping = zip splitted final_probs
+    let mapping = zip (src_test_data ++ lang_test_data) final_probs
 
-    print (mapping)
+    let test_accuracy_mapping = zip yTest final_probs
 
-    print "done" 
-    print 1
-    
-    {-
+    print mapping
+
+    print test_accuracy_mapping
+
+
+    print "DONE" {-
+
+
+
+    -- can have smth like every sequence of 0s or 1s is separated by a newline to demarcate separate segments or smth
+
+    -- need error percentages and other stuff like recall precision and stuff so that can make nice tables and stuff. Also need to cross-validate stuff
+
+
+    let code_class = [x | x <- mapping, snd x == 0]
+    let lang_class = [x | x <- mapping, snd x == 1]
+
+    writeFile "output_files/NB_lang_class.txt" (unlines (map fst lang_class))
+    writeFile "output_files/NB_code_class.txt" (unlines (map fst code_class))
+
+
+    -- print (mapping)
+
+
     let prob2 = DM.mapCol (+ logp) 1 
     let prob3 = DM.mapCol (\x -> x + log_not_p) 2 prob2
 
